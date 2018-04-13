62c390ba8347709c19051ebf9466d677366f9f43
Nancy He <lurong.he@williamoneilchina.com>
2018-04-13 18:40:14 +0800

    update

--- Cookie/login.js
+++ Cookie/login.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var connectDB = require("./model/db.js");
+var crypto = require("crypto");
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,18 +13,60 @@ app.use(session({
 }));
 
 app.get('/', function (req,res) { 
+    if (req.session.isLogin) {
+        res.send("Welcome back: " + req.session.username);
+    } else { 
+        res.send("Please log in");
+    }
+});
+
+app.get('/login', function (req, res) {
     res.render("login");
 });
 
-app.post("/login", function (req, res) {
+app.post('/register', function (req, res) { 
+    var username = req.body.username;
+    var password = req.body.password;
+    if (username && password) {
+        //encrypt password by md5
+        var hash = crypto.createHash('md5');
+        
+
+        connectDB(function (client,db) {
+            db.collection("users").insertOne({username:username});
+        });
+    }
+});
+
+
+app.post("/login", function (req, res, next) {
     console.log(req.body);
     var username = req.body.username;
     var password = req.body.password;
-    res.end();
+    if (username && password) {
+        connectDB(function (client, db) {
+            db.collection("users").find({ "username": username }).toArray(function (err, documents) {
+                if (err) {
+                    next(err);
+                }
+                console.log("documents", documents);
+                if (documents.length === 0) {
+                    return next(new Error("No account"));
+                }
+                if (documents[0].password === password) {
+                    req.session.isLogin = true;
+                    req.session.username = username;
+                    res.send("Login success,welcome back: "+ username);
+                } else { res.send("wrong password"); }
+                client.close();
+            })
+        });
+    }
 });
 
-app.use(function (err) { 
-    console.error(err);
+app.use(function (err, req, res, next) {
+    console.log(err);
+    res.send(err);
 });
 
 app.listen(12345);
--- Cookie/model/db.js
+++ Cookie/model/db.js
@@ -1,13 +1,12 @@
 var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
 var url = 'mongodb://localhost:27017';
 
 var connectDB = function (callback) {
     MongoClient.connect(url, function (err, client) {
-        if (err) { 
-            next(err);
-        }
+        assert.equal(err,null);
         var db = client.db('test');
-        callback(db);
+        callback(client,db);
     });
 };
 module.exports = connectDB;
